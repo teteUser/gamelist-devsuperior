f528717942f5c92d9a2e3e3ca59bda2e250cddde
Tennison Capra <tennison_capra@hotmail.com>
2023-05-10 14:33:21 -0300

    created GameList, Belonging and BelongingPK classes in the entity layer

---
+++ src/main/java/br/com/tete/gamelist/entity/BelongingPK.java
@@ -0,0 +1,59 @@
+package br.com.tete.gamelist.entity;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class BelongingPK implements Serializable {
+
+    private static final long serialVersionUID = -2474859459253124159L;
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList list;
+
+    public BelongingPK() {
+    }
+
+    public BelongingPK(Game game, GameList list) {
+        this.game = game;
+        this.list = list;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getList() {
+        return list;
+    }
+
+    public void setList(GameList list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, list);
+    }
+
+}
